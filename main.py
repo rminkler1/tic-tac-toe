419b4f1aa04075d92fd4da157ed1b12983890987
robertminkler <robertminkler@gmail.com>
2024-02-24 11:40:00 -0700

    Moved board class to seperate file. Implemented unitest tests on check_for_win

--- main.py
+++ main.py
@@ -1,78 +1,4 @@
-# Build game board and valid character list
-class Board:
-    def __init__(self, player_x, player_o, curr_player="X"):
-        self.board = [['A', 'B', 'C'], ['D', 'E', 'F'], ['G', 'H', 'I']]
-        self.valid_positions = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I']
-        self.curr_player = curr_player
-        self.player_x_name = player_x
-        self.player_o_name = player_o
-        self.game_on = True
-        self.winner = ''
-
-    # Display board
-    def show(self):
-        for row in self.board:
-            print(f"|{row[0]}|{row[1]}|{row[2]}|")
-
-    # Update board with user input
-    def update(self, selection):
-        # remove selection from valid positions
-        self.valid_positions.remove(selection)
-
-        # place x or o on the board
-        for row in self.board:
-            if selection in row:
-                i = row.index(selection)
-                row[i] = self.curr_player
-
-    # get user input
-    def prompt_player(self):
-        # get player name for prompt
-        if self.curr_player == "X":
-            player_name = self.player_x_name
-        else:
-            player_name = self.player_o_name
-
-        # get user input
-        data = input(f"{player_name}, select the box to place your mark. ").upper()
-
-        # validate user input
-        if data not in self.valid_positions or data == '':
-            print("\n\nInvalid Input. Please select a valid location.\n")
-            return None
-        else:
-            return data
-
-    # flip players between turns
-    def switch_player(self):
-        if self.curr_player == "X":
-            self.curr_player = "O"
-        else:
-            self.curr_player = "X"
-
-    def check_for_win(self):
-        # if all spaces are used end game
-        if not self.valid_positions:
-            self.game_on = False
-
-        # check rows
-        for row in self.board:
-            if row[0] == row[1] == row[2]:
-                self.game_on = False
-                self.winner = row[0]
-
-        # check columns
-        for col in range(3):
-            if self.board[0][col] == self.board[1][col] == self.board[2][col]:
-                self.game_on = False
-                self.winner = self.board[0][col]
-
-        # check diag
-        if (self.board[0][0] == self.board[1][1] == self.board[2][2] or
-                self.board[0][2] == self.board[1][1] == self.board[2][0]):
-            self.game_on = False
-            self.winner = self.board[1][1]
-
+from board import Board
 
 # Get Player's Names
 playerX = input("Player X, what is your name?\n")
@@ -96,14 +22,4 @@ while board.game_on:
     board.check_for_win()
 
 # end of game message
-board.show()
-if board.winner == "X":
-    name = board.player_x_name
-else:
-    name = board.player_o_name
-
-if board.winner:
-    print(f"{board.winner}'s won!")
-    print(f"Great job {name}!")
-else:
-    print("CATS! Try Again.")
+board.end()
---
+++ tests_board.py
@@ -0,0 +1,85 @@
+import unittest
+from board import Board
+
+
+class TestBoard(unittest.TestCase):
+    """ Test Cases for the Board class in board.py """
+    def test_check_for_win_example_01(self):
+        """ starting board. game continues """
+        board = Board(player_x='playerX', player_o='playerO')
+        board.board = [['A', 'B', 'C'], ['D', 'E', 'F'], ['G', 'H', 'I']]
+        actual = board.check_for_win()
+        expected = True
+        self.assertEqual(actual, expected)
+
+    def test_check_for_win_example_02(self):
+        """ col 1 win. game ends """
+        board = Board(player_x='playerX', player_o='playerO')
+        board.board = [['X', 'B', 'C'], ['X', 'E', 'F'], ['X', 'H', 'I']]
+        actual = board.check_for_win()
+        expected = False
+        self.assertEqual(actual, expected)
+
+    def test_check_for_win_example_03(self):
+        """ col 2 win. game ends """
+        board = Board(player_x='playerX', player_o='playerO')
+        board.board = [['A', 'X', 'C'], ['D', 'X', 'F'], ['G', 'X', 'I']]
+        actual = board.check_for_win()
+        expected = False
+        self.assertEqual(actual, expected)
+
+    def test_check_for_win_example_04(self):
+        """ col 3 win. game ends """
+        board = Board(player_x='playerX', player_o='playerO')
+        board.board = [['A', 'B', 'O'], ['D', 'E', 'O'], ['G', 'H', 'O']]
+        actual = board.check_for_win()
+        expected = False
+        self.assertEqual(actual, expected)
+
+    def test_check_for_win_example_05(self):
+        """ row 1 win. game ends """
+        board = Board(player_x='playerX', player_o='playerO')
+        board.board = [['O', 'O', 'O'], ['D', 'E', 'F'], ['G', 'H', 'I']]
+        actual = board.check_for_win()
+        expected = False
+        self.assertEqual(actual, expected)
+
+    def test_check_for_win_example_06(self):
+        """ row 2 win. game ends """
+        board = Board(player_x='playerX', player_o='playerO')
+        board.board = [['A', 'X', 'C'], ['O', 'O', 'O'], ['G', 'H', 'I']]
+        actual = board.check_for_win()
+        expected = False
+        self.assertEqual(actual, expected)
+
+    def test_check_for_win_example_07(self):
+        """ row 3 win. game ends """
+        board = Board(player_x='playerX', player_o='playerO')
+        board.board = [['A', 'B', 'C'], ['D', 'E', 'F'], ['O', 'O', 'O']]
+        actual = board.check_for_win()
+        expected = False
+        self.assertEqual(actual, expected)
+
+    def test_check_for_win_example_08(self):
+        """ dig 1 win. game ends """
+        board = Board(player_x='playerX', player_o='playerO')
+        board.board = [['X', 'B', 'C'], ['D', 'X', 'F'], ['G', 'H', 'X']]
+        actual = board.check_for_win()
+        expected = False
+        self.assertEqual(actual, expected)
+
+    def test_check_for_win_example_09(self):
+        """ diag 2 win. game ends """
+        board = Board(player_x='playerX', player_o='playerO')
+        board.board = [['A', 'B', 'O'], ['D', 'O', 'F'], ['O', 'H', 'I']]
+        actual = board.check_for_win()
+        expected = False
+        self.assertEqual(actual, expected)
+
+    def test_check_for_win_example_10(self):
+        """ cats - no remaining valid positions. game ends """
+        board = Board(player_x='playerX', player_o='playerO')
+        board.valid_positions = []
+        actual = board.check_for_win()
+        expected = False
+        self.assertEqual(actual, expected)
